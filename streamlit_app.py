418b02be20a94c8b39605b9693f375320b2197fb
Hasan Çatalgöl <39779409+hasancatalgol@users.noreply.github.com>
2022-04-20 14:48:09 +0300

    Create skeleton of the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,27 @@
+#Import Libraries
 import streamlit as st
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+#Read Data and join both tables
+users = pd.read_csv('Data/users.csv')
+purchases = pd.read_csv('Data/purchases.csv')
+df = purchases.merge(users, how = "inner", on = "uid")
 
 
-st.write("hello")
+
+label_country = st.selectbox('Country', ('TUR', 'BRA', 'DEU', 'USA', 'FRA','CAN'))
+label_device = st.selectbox('Device', ('AND', 'IOS'))
+label_gender = st.selectbox('Gender', ('F', 'M'))
+label_age = st.slider('How old are you?', 0, 80, 50)
+
+
+
+
+
+prediction = print(str(label_country) + "_" + str(label_device) + "_" + str(label_gender) + "_"+ str(label_age))
+st.button(label="Predict" , on_click = prediction)
+
+
+
